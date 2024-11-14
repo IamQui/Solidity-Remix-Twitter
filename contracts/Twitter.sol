ce34bebfc4a485881b25a873d6a92b61bf7a79e0
IamQui <iamquicode@gmail.com>
2024-11-14 16:39:57 -0600

    twitter-remix

---
+++ contracts/Twitter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Twitter {
+
+    mapping(address => string) public tweets;
+
+    function createtweet(string memory _tweet) public {
+        tweets[msg.sender] = _tweet;
+    }
+
+    function getTweet(address _owner) public view returns(string memory){
+        return tweets[_owner];
+    }
+}
