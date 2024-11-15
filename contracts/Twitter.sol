c1d3b5ce82f8b1d74bb68429aefc9a69228dbd29
IamQui <iamquicode@gmail.com>
2024-11-15 06:49:06 -0600

    Added-custom-modifier

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.26;
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_LENGTH = 280;
+    uint16 public MAX_TWEET_LENGTH = 280;
 
     struct Tweet {
         address author;
@@ -14,10 +14,23 @@ contract Twitter {
 
     mapping(address => Tweet[]) public tweets;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER!");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
+
     function createtweet(string memory _tweet) public {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "You shall not pass!");
-
-
 
         Tweet memory newTweet = Tweet({
             author: msg.sender,
